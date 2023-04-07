86e1999b68d934fd94b1350add7c1cd98adcb186
anotherITdude <abhi.baidya@gmail.com>
2023-04-07 15:51:01 +0200

    apiclient setup for games list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,30 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { Grid, GridItem, Show } from '@chakra-ui/react'
-import Navbar from './components/Navbar'
-import GameList from './components/GameList'
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import GameList from "../GameList";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        md: `"nav nav" "aside main"`,
-      }}
-    >
-      <GridItem area="nav">
-        <Navbar />
-      </GridItem>
-      <Show above="md">
-        <GridItem area="aside">
-          Aside
+        templateAreas={{
+          base: `"nav" "main"`,
+          md: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav">
+          <Navbar />
         </GridItem>
-      </Show>
-      <GridItem area="main">
-        <GameList />
-      </GridItem>
-    </Grid>
+        <Show above="md">
+          <GridItem area="aside">Aside</GridItem>
+        </Show>
+        <GridItem area="main">
+          <GameList />
+        </GridItem>
+      </Grid>
     </>
-  )
+  );
 }

306813aa7fe389066d26b95683aabe59dc5f555b
anotherITdude <abhi.baidya@gmail.com>
2023-04-07 16:27:43 +0200

    pre rendetring hook issue resolved for NextJS

--- src/pages/components/GameList.tsx
+++ src/pages/components/GameList.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
-import apiClient from "@/services/api-client";
 import { Text } from "@chakra-ui/react";
-import useGames from '../hooks/useGames';
+import useGames from "../../hooks/useGames";
 
 const GameList = () => {
-  const{games, error }=useGames();
+  const { games, error } = useGames();
   return (
     <div>
       {error && <Text>{error}</Text>}

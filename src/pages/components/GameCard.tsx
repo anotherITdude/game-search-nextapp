295c383757726313c34009bb375f9f8c1ea4f744
anotherITdude <abhi.baidya@gmail.com>
2023-04-08 20:46:59 +0200

    game card issue resolved

--- src/pages/components/GameCard.tsx
+++ src/pages/components/GameCard.tsx
@@ -1,4 +1,5 @@
 import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+//import Image from "next/image";
 import React from "react";
 import { Game } from "../../hooks/useGames";
 
@@ -9,9 +10,9 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow={"hidden"}>
-      <Image src={game.background_image} />
+      <Image width="auto" alt={game?.name} src={game?.background_image} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
+        <Heading fontSize="2xl">{game?.name}</Heading>
       </CardBody>
     </Card>
   );

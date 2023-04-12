5bab7fe87e0d52f4cf65fdd6258e8518104c943b
anotherITdude <abhi.baidya@gmail.com>
2023-04-12 13:07:40 +0200

    navbar logo refactored

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -1,14 +1,18 @@
-import { HStack } from "@chakra-ui/react";
+import { Box, HStack, Icon, Text } from "@chakra-ui/react";
 import Image from "next/image";
 import React from "react";
 import logo from "../../../public/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import { FaCode } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <div>
-      <HStack justifyContent="space-between" padding="0px 15px 20px 15px">
-        <Image priority={true} alt="logo" width={50} src={logo} />
+      <HStack justifyContent="space-between" padding="20px 15px 20px 15px" borderBottom={'solid 0.5px'} marginBottom={10}>
+        <HStack>
+          <Icon  boxSize={10} fontSize={34} as={FaCode} />
+          <Text>NextJS Game App</Text>
+        </HStack>
         <ColorModeSwitch />
       </HStack>
     </div>

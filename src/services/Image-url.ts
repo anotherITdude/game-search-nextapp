f3ee860e10e2532ea92553a425b874057c3c1548
anotherITdude <abhi.baidya@gmail.com>
2023-04-12 15:43:50 +0200

    handle no image game implemented

--- src/services/Image-url.ts
+++ src/services/Image-url.ts
@@ -1,7 +1,8 @@
+import noImage from "./../../public/no-image.webp";
 const getCroppedImage = (url: string) => {
-    if (!url) return '';
-    const target = 'media/';
-    const index = url.indexOf(target) + target.length;
-    return url.slice(0,index) +'crop/600/400/'+ url.slice(index)
-} 
-export default getCroppedImage
+  if (!url) return noImage.src;
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
+export default getCroppedImage;

44e24fe8ece6cc55212bd793710a6725a370a028
anotherITdude <abhi.baidya@gmail.com>
2023-04-12 17:06:49 +0200

    search input added

--- src/pages/components/ColorModeSwitch.tsx
+++ src/pages/components/ColorModeSwitch.tsx
@@ -10,7 +10,7 @@ const ColorModeSwitch = () => {
           isChecked={colorMode === "dark"}
           colorScheme="green"
         ></Switch>
-        <Text>Dark Mode</Text>
+        <Text whiteSpace='nowrap'>Dark Mode</Text>
       </HStack>
     </div>
   );
--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -2,15 +2,15 @@ import {HStack, Icon, Text } from "@chakra-ui/react";
 import React from "react";
 import ColorModeSwitch from "./ColorModeSwitch";
 import { FaCode } from "react-icons/fa";
+import SearchInput from "./SearchInput";
 
 const Navbar = () => {
   return (
     <div>
       <HStack boxShadow={'xl'} justifyContent="space-between" padding="20px 15px 20px 15px" borderBottom={'solid 0.5px'} marginBottom={10}>
-        <HStack>
           <Icon  boxSize={10} fontSize={34} as={FaCode} />
-          <Text>NextJS Game App</Text>
-        </HStack>
+          <Text whiteSpace='nowrap'>NextJS Game App</Text>
+          <SearchInput />
         <ColorModeSwitch />
       </HStack>
     </div>
